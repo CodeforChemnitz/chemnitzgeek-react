0cfd5a8477a1375217ee12bdca5ee6468ddb3243
Benedikt Geissler <benedikt@g5r.eu>
2017-07-05 02:34:49 +0200

    Move css, images and tests into separate directories

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
-import './App.css';
+import './style/App.css';
 
-const logo = require('./logo.svg');
+const logo = require('./style/logo.svg');
 
 class App extends React.Component<{}, {}> {
   render() {
