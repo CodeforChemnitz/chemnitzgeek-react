6ffe627fc38e5b1f041a9e08070524f89d916334
Benedikt Geissler <benedikt@g5r.eu>
2017-07-09 02:28:21 +0200

    Add "almost hot reloading"

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,16 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'mini.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('container') as HTMLElement
-);
+const rootElem = document.getElementById('container') as HTMLElement;
+ReactDOM.render(<App/>, rootElem);
+
+// tslint:disable-next-line:no-any
+if ((module as any).hot) {
+  // tslint:disable-next-line:no-any
+  (module as any).hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    ReactDOM.render(<NextApp/>, rootElem);
+  });
+}
+
 registerServiceWorker();
