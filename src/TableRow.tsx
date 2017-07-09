427508da137f873d52520cfcc335148d5bba8bdf
Benedikt Geissler <benedikt@g5r.eu>
2017-07-09 06:21:19 +0200

    Visualize the rating and weight

--- src/TableRow.tsx
+++ src/TableRow.tsx
@@ -9,10 +9,14 @@ export default function TableRow(props: TableRowProps) {
   return (
     <tr>
       <td data-label="Name">{props.localName}</td>
-      <td data-label="Bewertung">{props.rating}</td>
+      <td data-label="Bewertung">
+        <progress value={props.rating} title={String(props.rating)} min={1} max={10}/>
+      </td>
       <td data-label="Spieleranzahl">{playerCount}</td>
       <td data-label="Alter">{props.minAge}+</td>
-      <td data-label="Gewicht">{props.weight}</td>
+      <td data-label="Gewicht">
+        <progress value={props.weight} title={String(props.weight)} min={1} max={5}/>
+      </td>
       <td data-label="Jahr">{props.yearPublished}</td>
       <td data-label="Quelle">{props.sources.join(', ')}</td>
     </tr>
